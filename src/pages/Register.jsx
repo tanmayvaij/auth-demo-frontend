211b824c34a7813f58a4ab529318bceef4f4c216
Tanmay Vaij <tanmayvaij22@gmail.com>
2022-12-29 18:34:13 +0530

    completed register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import { GlobalStates } from "../context"
+import { useState } from "react"
 
 export default function Register() {
+
+    const { SERVER_ADDRESS } = GlobalStates()
+
+    const [ state, setState ] = useState({
+        name: "", number: "", email: "", address: "", password: "", cpassword: ""
+    })
+
+    const register = async () => {
+        
+        const res = await fetch(`${SERVER_ADDRESS}/api/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(state)
+        })
+
+        const data = await res.json()
+
+        if (data.success) {
+            localStorage.setItem("authtoken", data.authtoken)
+            location.reload()
+        }
+
+        else alert(data.message)
+
+    }
+
     return (
         <div id="registration" className="min-h-screen flex items-center justify-center">
             <div className="border bg-white rounded-md shadow-md flex items-center justify-center flex-col space-y-8 px-10 py-10">
@@ -13,38 +43,85 @@ export default function Register() {
 
                     <div>
                         <i className="fa-solid fa-user"></i>
-                        <input type="text" className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" placeholder="Type your name" />
+                        <input 
+                            name="name"
+                            type="text" 
+                            className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none"
+                            placeholder="Type your name" 
+                            value={state.name}
+                            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value})}
+                        />
                     </div>
 
                     <div>
                         <i className="fa-solid fa-phone"></i>
-                        <input type="tel" className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" placeholder="Type your phone number" />
+                        <input 
+                            name="number"
+                            type="tel" 
+                            className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" 
+                            placeholder="Type your phone number" 
+                            value={state.number}
+                            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value})}
+                        />
                     </div>
 
                     <div>
                         <i className="fa-solid fa-envelope"></i>
-                        <input type="email" className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" placeholder="Type your email" />
+                        <input 
+                            name="email"
+                            type="email" 
+                            className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" 
+                            placeholder="Type your email" 
+                            value={state.email}
+                            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value})}
+                        />
                     </div>
 
                     <div>
                         <i className="fa-solid fa-location-dot"></i>
-                        <input type="text" className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" placeholder="Type your address" />
+                        <input 
+                            name="address"
+                            type="text" 
+                            className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none"
+                            placeholder="Type your address"
+                            value={state.address}
+                            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value})}
+                        />
                     </div>
 
                     <div>
                         <i className="fa-solid fa-lock"></i>
-                        <input type="password" className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" placeholder="Type your password" />
+                        <input 
+                            name="password"
+                            type="password" 
+                            className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" 
+                            placeholder="Type your password" 
+                            value={state.password}
+                            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value})}
+                        />
                     </div>
 
                     <div>
                         <i className="fa-solid fa-lock"></i>
-                        <input type="password" className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" placeholder="Confirm your password" />
+                        <input 
+                            name="cpassword"
+                            type="password" 
+                            className="transition delay-200 focus:border-b-pink-500 border-b-2 border-b-gray-200 text-sm pl-3 h-10 w-60 focus:!outline-none" 
+                            placeholder="Confirm your password" 
+                            value={state.cpassword}
+                            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value})}
+                        />
                     </div>
 
                 </div>
 
                 <div className="w-full">
-                    <button className="my-5 bg-pink-600 hover:bg-pink-500 rounded-md p-2 text-white font-medium w-full">Register</button>
+                    <button 
+                        className="my-5 bg-pink-600 hover:bg-pink-500 rounded-md p-2 text-white font-medium w-full"
+                        onClick={register}
+                    >
+                        Register
+                    </button>
                 </div>
 
                 <p className="text-sm">
